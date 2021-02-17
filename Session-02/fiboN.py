d95cf2aaf0bebb0bd477dd80638ea37205c9926c
pmglez <p.martinezg.2020@alumnos.urjc.es>
2021-02-17 19:55:43 +0100

    Changes in fiboN.py

--- Session-02/fiboN.py
+++ Session-02/fiboN.py
@@ -1,15 +1,18 @@
 def fibon(n):
     n1 = 0
     n2 = 1
-    for i in range(0, n+1):
-        num = i
-        if i > 1:
-            num = n1 + n2
-            # new values
-            n1 = n2
-            n2 = num
-        print(num)
+    fibonacci_list = [n1, n2]
+    for i in range(0, (n - 1)):
+        num = n1 + n2
+        fibonacci_list.append(num)
+
+        # new values
+        n1 = n2
+        n2 = num
+
+    return fibonacci_list[-1]
 
 
-
-print("Fibonacci Sequence: ", fibon(20))
+print("5th Fibonacci term: ", fibon(5))
+print("10th Fibonacci term: ", fibon(10))
+print("15th Fibonacci term: ", fibon(15))
