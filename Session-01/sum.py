64b465d5748e68e85f10f075c3a64dd66df51d24
pmglez <p.martinezg.2020@alumnos.urjc.es>
2021-02-17 19:10:07 +0100

    Changes

--- Session-01/sum.py
+++ Session-01/sum.py
@@ -1,7 +1,9 @@
 res = 0
 
+# res: result == sum/addition
+
 for i in range(1, 21):
-    # print("Now the result is :", res)
+    # print("Now the result is :", addition)
     res += i
 
 print("TOTAL SUM :", res)
--- Session-01/sumN.py
+++
@@ -1,9 +0,0 @@
-def addition(n):
-    res = 0
-    for i in range(1, n+1):
-        res += i
-    return res
-
-
-print("Sum of the 20 first integers: ", addition(20))
-print("Sum of the 100 first integers: ", addition(100))
