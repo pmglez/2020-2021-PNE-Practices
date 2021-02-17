15873c78b589986053b70aced8be43c10f764a22
pmglez <p.martinezg.2020@alumnos.urjc.es>
2021-02-17 19:44:42 +0100

    New Changes

--- Session-02/fibonacci.py
+++ Session-02/fibonacci.py
@@ -1,10 +1,14 @@
 n1 = 0
 n2 = 1
-for i in range(0, 11):
-    num = i
-    if i > 1:
-        num = n1 + n2
-        # new values
-        n1 = n2
-        n2 = num
-    print(num, end=" ")
+fibonacci_list = [n1, n2]
+for i in range(0, (11 - len(fibonacci_list))):
+    num = n1 + n2
+    fibonacci_list.append(num)
+
+    # new values
+    n1 = n2
+    n2 = num
+
+# print list
+for i in fibonacci_list:
+    print(i, end=" ")
