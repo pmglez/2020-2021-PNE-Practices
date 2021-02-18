e88a96389ef6e83383b3b3864e4c189ebe91be80
pmglez <p.martinezg.2020@alumnos.urjc.es>
2021-02-18 12:22:42 +0100

    Finding the error debug_me.py

--- Session-03/debug_me.py
+++ Session-03/debug_me.py
@@ -1,6 +1,5 @@
 # --- Find the error!
 
-
 def g(a, b):
     return a - b
 
@@ -8,12 +7,15 @@ def g(a, b):
 def f(a, b, c, d):
     t0 = a + b - g(a, 0)
     t1 = g(c, d)
-    t3 = 2 * (t0 / t1)
-    return t0 + 2*t1 + t3*t3
+    if c != d:
+        t3 = 2 * (t0 / t1)
+        return t0 + 2 * t1 + t3 * t3
+    else:
+        return "Cannot calculate that. c and d must be different"
 
 
 # -- Main program
 print("Result 1: ", f(5, 2, 5, 0))
-#print("Result 2: ", f(0, 2, 3, 3))
+print("Result 2: ", f(0, 2, 3, 3))
 print("Result 3: ", f(1, 3, 2, 3))
 print("Result 4: ", f(1, 9, 22.0, 3))
